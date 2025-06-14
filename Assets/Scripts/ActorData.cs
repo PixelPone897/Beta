b55ee98080cffe14daf5710c832cfe17c946a5bc
PixelPone897 <mattjroman03@gmail.com>
2025-06-13 21:01:18 -0400

    Implement basic data storage for Actors

---
+++ Assets/Scripts/ActorData.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using static Scripts.Constants;
+
+namespace Scripts.Actors
+{
+    [CreateAssetMenu(fileName = "NewActorData", menuName = "Actors/ActorData", order = 1)]
+    public class ActorData : ScriptableObject
+    {
+        [field: SerializeField, Header("General Info")] 
+        public string ActorName { get; private set; } = DUMMY_STRING;
+        [field: SerializeField, TextArea]
+        public string ActorDescription { get; private set; } = DUMMY_STRING;
+        [SerializeField]
+        public ActorGender Gender { get; private set; } = ActorGender.MALE;
+
+        [field: SerializeField]
+        public ActorRace Race { get; private set; } = ActorRace.EARTH;
+
+        //Special Stats
+        [field: SerializeField, Header("SPECIAL Values")]
+        public int Strength { get; private set; } = 5;
+        [field: SerializeField]
+        public int Perception { get; private set; } = 5;
+        [field: SerializeField]
+        public int Endurance { get; private set; } = 5;
+        [field: SerializeField]
+        public int Charisma { get; private set; } = 5;
+        [field: SerializeField]
+        public int Intelligence { get; private set; } = 5;
+        [field: SerializeField]
+        public int Agility { get; private set; } = 5;
+        [field: SerializeField]
+        public int Luck { get; private set; } = 5;
+
+        public void Reset()
+        {
+            Strength = 5;
+            Perception = 5;
+            Endurance = 5;
+            Charisma = 5;
+            Intelligence = 5;
+            Agility = 5;
+            Luck = 5;
+
+            //Debug.Log("On BaseActorStat Reset!");
+            PrintSpecialValues();
+        }
+
+        private void PrintSpecialValues()
+        {
+            Debug.Log($"Strength {Strength}");
+            Debug.Log($"Perception {Perception}");
+            Debug.Log($"Endurance {Endurance}");
+            Debug.Log($"Charisma {Charisma}");
+            Debug.Log($"Intelligence {Intelligence}");
+            Debug.Log($"Agility {Agility}");
+            Debug.Log($"Luck {Luck}");
+        }
+    }
+}
---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Scripts
+{
+    public static class Constants
+    {
+        public static readonly string DUMMY_STRING = "DUMMY_STRING";
+        public enum ActorGender
+        {
+            MALE,
+            FEMALE
+        }
+
+        public enum ActorRace
+        {
+            EARTH,
+            PEGASUS,
+            UNICORN
+        }
+
+    }
+}
