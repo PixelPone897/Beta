05de96e9cde9564b7750db8b0f35c4e9928d6f45
PixelPone897 <mattjroman03@gmail.com>
2025-07-14 19:45:11 -0400

    Implement required removal method for Effects

--- Assets/Scripts/Perks/Effect.cs
+++ Assets/Scripts/Perks/Effect.cs
@@ -1,4 +1,3 @@
-using Scripts;
 using Scripts.Actors;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,10 +8,11 @@ namespace Scripts.Perks
     public interface IPerkEffect
     {
         public abstract void ApplyEffect(GameObject actor);
+        public abstract void RemoveEffect(GameObject actor);
     }
 
     [System.Serializable]
-    public class PlusFivePerkEffect : IPerkEffect
+    public class PlusFiveSkillEffect : IPerkEffect
     {
         [SerializeField]
         private List<SkillName> skillsToAddTo;
@@ -21,7 +21,7 @@ namespace Scripts.Perks
         [SerializeField]
         private SkillName selectedPerk;
 
-        public PlusFivePerkEffect() { }
+        public PlusFiveSkillEffect() { }
 
         public void ApplyEffect(GameObject actor)
         {
@@ -29,6 +29,105 @@ namespace Scripts.Perks
             {
                 var modifier = new StatModifier(toAddToPerk, true);
                 stats.Skills[selectedPerk].AddPlusModifier(modifier);
+            }
+        }
+
+        public void RemoveEffect(GameObject actor)
+        {
+            if (actor.TryGetComponent<ActorSpecialStats>(out var stats))
+            {
+                var modifier = new StatModifier(-toAddToPerk, true);
+                stats.Skills[selectedPerk].RemovePlusModifier(modifier);
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class ModifySpecialEffect: IPerkEffect
+    {
+        [SerializeField]
+        private SpecialName specialName;
+        [field: SerializeField]
+        public List<StatModifier> PlusModifiers;
+        [field: SerializeField]
+        public List<StatModifier> MultiplyModifiers;
+
+        public ModifySpecialEffect() { }
+        public ModifySpecialEffect(SpecialName specialName)
+        {
+            this.specialName = specialName;
+            PlusModifiers = new List<StatModifier>();
+            MultiplyModifiers = new List<StatModifier>();
+        }
+
+        public Stat GetSpecialStat(ActorSpecialStats stats)
+        {
+            Stat specialStat;
+            switch (specialName)
+            {
+                case SpecialName.STRENGTH:
+                    specialStat = stats.Strength;
+                    break;
+                case SpecialName.PERCEPTION:
+                    specialStat = stats.Perception;
+                    break;
+                case SpecialName.ENDURANCE:
+                    specialStat = stats.Endurance;
+                    break;
+                case SpecialName.CHARISMA:
+                    specialStat = stats.Charisma;
+                    break;
+                case SpecialName.INTELLIGENCE:
+                    specialStat = stats.Intelligence;
+                    break;
+                case SpecialName.AGILITY:
+                    specialStat = stats.Agility;
+                    break;
+                case SpecialName.LUCK:
+                    specialStat = stats.Luck;
+                    break;
+                default:
+                    specialStat = stats.Strength;
+                    break;
+            }
+            return specialStat;
+        }
+
+        public void ApplyEffect(GameObject actor)
+        {
+            if (actor.TryGetComponent<ActorSpecialStats>(out var stats))
+            {
+
+                Stat statToModify = GetSpecialStat(stats);
+
+                foreach (var modifier in PlusModifiers)
+                {
+                    statToModify.AddPlusModifier(modifier);
+                }
+
+                foreach (var modifier in MultiplyModifiers)
+                {
+                    statToModify.RemoveMultiplyModifier(modifier);
+                }
+            }
+        }
+
+        public void RemoveEffect(GameObject actor)
+        {
+            if (actor.TryGetComponent<ActorSpecialStats>(out var stats))
+            {
+
+                Stat statToModify = GetSpecialStat(stats);
+
+                foreach (var modifier in PlusModifiers)
+                {
+                    statToModify.RemovePlusModifier(modifier);
+                }
+
+                foreach (var modifier in MultiplyModifiers)
+                {
+                    statToModify.RemoveMultiplyModifier(modifier);
+                }
             }
         }
     }
