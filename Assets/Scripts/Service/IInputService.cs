7fc27668c8d8b7af80d9429dcceef03359d58f31
PixelPone897 <mattjroman03@gmail.com>
2025-08-01 13:25:44 -0400

    Implement proper documentation

--- Assets/Scripts/Service/IInputService.cs
+++ Assets/Scripts/Service/IInputService.cs
@@ -3,10 +3,30 @@ using UnityEngine;
 
 namespace Assets.Scripts.Service
 {
+    /// <summary>
+    /// Interface for handling input.
+    /// </summary>
+    /// <remarks>This is used to abstract "input" sources (direct player input vs
+    /// AI methods for example) so that they can be handled in a similar
+    /// manner.</remarks>
     internal interface IInputService
     {
+        /// <summary>
+        /// Event triggered when "move" input is retrived (either through
+        /// direct player input, an AI method, etc).
+        /// </summary>
         public event EventHandler<Vector2> OnMoveInput;
+
+        /// <summary>
+        /// Enables input service.
+        /// </summary>
+        /// <remarks>Should be used for enabling and subscribing to EventHandlers.</remarks>
         public abstract void EnableInput();
+
+        /// <summary>
+        /// Disables input service.
+        /// </summary>
+        /// <remarks>Should be used for disabling and unsubscribing to EventHandlers.</remarks>
         public abstract void DisableInput();
     }
 }
--- Assets/Scripts/Service/PlayerCombatInputService.cs
+++ Assets/Scripts/Service/PlayerCombatInputService.cs
@@ -4,10 +4,23 @@ using static UnityEngine.InputSystem.InputAction;
 
 namespace Assets.Scripts.Service
 {
+    /// <summary>
+    /// Service that handles input from the player.
+    /// </summary>
     public class PlayerCombatInputService : IInputService
     {
         public event EventHandler<Vector2> OnMoveInput;
 
+        /// <summary>
+        /// Input Actions used by Unity to handle player input events.
+        /// </summary>
+        /// <remarks>
+        /// Note- events are useful for responding to when inputs are pressed.
+        /// However, they will only be triggered once! If you want to have
+        /// repeated logic run while an input is pressed, it is better to
+        /// poll for an action in a Update() method rather than listening
+        /// to an event.
+        /// </remarks>
         private TestInput inputActions;
 
         public void EnableInput()
