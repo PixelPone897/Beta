08faa835144e44092cb309b397dc60c1aab47d6d
PixelPone897 <mattjroman03@gmail.com>
2025-08-01 20:18:43 -0400

    Implement base class for CombatAction

---
+++ Assets/Scripts/Combat/CombatActions/CombatAction.cs
@@ -0,0 +1,138 @@
+﻿using Assets.Scripts.Services;
+using Scripts.Actors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Combat.CombatActions
+{
+    /// <summary>
+    /// Represents an action in battle (taking a turn, shooting, etc).
+    /// </summary>
+    /// <remarks>
+    /// This was constructed and injected with the proper services/contexts through
+    /// a CombatActionData's BuildAction() method. If a CombatActionData is the
+    /// scafolding of a CombatAction, think of CombatAction as being constructed,
+    /// runtime version of that.
+    /// </remarks>
+    public abstract class CombatAction
+    {
+        /// <summary>
+        /// Represents how soon the CombatAction is due to execute.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        ///  The CombatAction with the lowest CombatAction value is the one that is set to execute next-
+        ///  it is removed from the CombatActionQueue and all the other CountDown values are decreased by one.
+        /// </para>
+        ///  <para>
+        ///  Note!- CountDown is only used to determine the order of a CombatAction, not how much
+        ///  time it will take for a CombatAction to run.
+        /// </para>
+        /// </remarks>
+        public int CountDown { get; set; }
+
+        /// <summary>
+        /// The duration of how long this CombatAction has been running for.
+        /// </summary>
+        public float CurrentDuration => Time.time - startTime;
+
+        /// <summary>
+        /// The time in which this CombatAction started.
+        /// </summary>
+        protected float startTime;
+
+        /// <summary>
+        /// The service provider injected into this action.
+        /// </summary>
+        /// <remarks>
+        /// This is passed into the CombatAction so that its children CombatSteps
+        /// can access it if needed.
+        /// </remarks>
+        public UnityServiceProvider ServiceProvider { get; private set; }
+
+        /// <summary>
+        /// Instance to the BattleManager.
+        /// </summary>
+        public BattleManager BattleManagerRef { get; private set; }
+
+        /// <summary>
+        /// Indicates "who" owns the CombatAction. Useful when removing all
+        /// CombatActions associated with a certain owner from
+        /// CombatActionQueue.
+        /// </summary>
+        public ActorSpecialStats Owner { get; private set; }
+
+        protected Queue<CombatStep> combatSteps;
+        protected CombatStep currentCombatStep;
+
+        public CombatAction(UnityServiceProvider serviceProvider)
+        {
+            ServiceProvider = serviceProvider;
+            currentCombatStep = null;
+        }
+
+        // Might change this so that BattleManager and Actor is injected via contexts
+        // instead, so that CanBePerformed() can be run first before running Start.
+        // Similar to how CombatStep is currently handled.
+
+        /// <summary>
+        /// Runs when the CombatAction first starts (can be used to set up values if needed).
+        /// </summary>
+        /// <remarks>Think of this like Monobehavior's OnEnable() method.</remarks>
+        /// <param name="bm">The instance to BattleManager.</param>
+        /// <param name="owner">The Actor who owns this CombatAction.</param>
+        public virtual void StartAction(BattleManager bm, ActorSpecialStats owner)
+        {
+            BattleManagerRef = bm;
+            Owner = owner;
+            startTime = Time.time;
+        }
+
+        /// <summary>
+        /// Updates logic of CombatAction every frame.
+        /// </summary>
+        /// <remarks>Think of this like Monobehavior's Update() method.</remarks>
+        public virtual void UpdateAction()
+        {
+            
+        }
+
+        /// <summary>
+        /// Cleans up logic associated with CombatAction.
+        /// </summary>
+        /// <remarks>Think of this like Monobehavior's OnDisable() method.</remarks>
+        public abstract void EndAction();
+
+        /// <summary>
+        /// Indicates if the CombatState is finished.
+        /// </summary>
+        /// <returns>
+        /// True- if the CombatState is finished.
+        /// False- if the CombatState is not finished.
+        /// </returns>
+        public abstract bool CanBePerformed();
+
+        /// <summary>
+        /// Indicates if the CombatState can be performed.
+        /// </summary>
+        /// <returns>
+        /// True- if the CombatState can be performed.
+        /// False- if the CombatState can not be performed.
+        /// </returns>
+        public abstract bool IsFinished();
+
+        /// <summary>
+        /// Automatically clears all registered context types from the service provider.
+        /// Call this at the end of this action to avoid leaking context into future actions.
+        /// </summary>
+        public void ClearContext()
+        {
+            ServiceProvider.ClearContexts();
+        }
+
+    }
+}
