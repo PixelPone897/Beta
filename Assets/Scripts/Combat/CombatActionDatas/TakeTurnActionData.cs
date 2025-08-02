236c2d110078ec9817fe0071b309315fb36fe930
PixelPone897 <mattjroman03@gmail.com>
2025-08-01 23:03:51 -0400

    Add temporary code for testing TakeTurnAction

---
+++ Assets/Scripts/Combat/CombatActionDatas/TakeTurnActionData.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts.Combat.CombatActions;
+using Assets.Scripts.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Combat.CombatActionDatas
+{
+    public class TakeTurnActionData : CombatActionData
+    {
+        public override CombatAction BuildAction(UnityServiceProvider serviceProvider)
+        {
+            TakeTurnAction newAction = new TakeTurnAction(serviceProvider);
+            newAction.CombatSteps = CreateSteps(newAction);
+            return newAction;
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Combat/CombatActions/TakeTurnAction.cs
@@ -0,0 +1,51 @@
+﻿using Assets.Scripts.Services;
+using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Combat.CombatActions
+{
+    public class TakeTurnAction : CombatAction
+    {
+        private IInputService inputService;
+        private bool isFinished;
+
+        public TakeTurnAction(UnityServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            inputService = serviceProvider.GetService<IInputService>();
+            isFinished = false;
+        }
+
+        public override void StartAction()
+        {
+            Debug.Log("ACTION STARTED!");
+            base.StartAction();
+            inputService.EnableInput();
+            inputService.OnMoveInput += InputService_OnMoveInput;
+            inputService.OnSelectInput += InputService_OnSelectInput;
+        }
+
+        private void InputService_OnSelectInput(object sender, bool e)
+        {
+            Debug.Log("BUTTON PRESSED!");
+            isFinished = true;
+        }
+
+        private void InputService_OnMoveInput(object sender, Vector2 e)
+        {
+            Debug.Log("TESTING! "+e);
+        }
+
+        public override void EndAction()
+        {
+            Debug.Log("ACTION ENDED!");
+            inputService.OnMoveInput -= InputService_OnMoveInput;
+            inputService.DisableInput();
+            ClearContext();
+        }
+
+        public override bool IsFinished()
+        {
+            return isFinished;
+        }
+    }
+}
